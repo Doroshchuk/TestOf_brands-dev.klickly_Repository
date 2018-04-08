b0c025795876f9f645cc7aa20d7c4a006fb2ed6b
ddoroshchuk <doroshukdashuly@mail.ru>
2018-04-08 11:51:00 +0300

    Completed testing all elements in header.

--- pages/objectsOfMainPage/Header.js
+++ pages/objectsOfMainPage/Header.js
@@ -1,9 +1,7 @@
 class Header {
     constructor() {
-        // this.loginBtn = element(by.xpath("//a[@href='/page/sign-in']"));
-        this.loginBtn = element(By.xpath("//a[@class='filled btn btn-base btn_transparent']"));
-        // this.signUpBtn = element(by.xpath("//a[@href='/page/sign-up']"));
-        this.signUpBtn = element(By.xpath("//a[@class='filled btn btn-base btn-base__cerise']"));
+        this.loginBtn = element(By.xpath("//a[@href='/page/sign-in']"));
+        this.signUpBtn = element(By.xpath("//a[@href='/page/sign-up']"));
         this.image = element(by.css("img[src*='/36dee93c6c9748b0255ce1240d63c2b7.svg']"));
         this.name = element(by.xpath("//span[@class='header-logo-text']"));
     }
--- tests/CheckingAllElements_spec.js
+++ tests/CheckingAllElements_spec.js
@@ -7,7 +7,27 @@ describe('Check all elements of Main page', function () {
         browser.get('https://brands-dev.klickly.com/');
     });
 
+    it('check that page url is right', function () {
+        expect(browser.driver.getTitle()).toBe("Klickly Brands");
+    });
+
+    it('check that text is appeared in header', function () {
+        expect(mainPage.header.name.isPresent()).toBeTruthy();
+    });
+
     it('check text in header', function () {
         expect(mainPage.header.getHeaderText()).toBe("Brands");
     });
+
+    it('check that image is appeared in header', function () {
+        expect(mainPage.header.image.isPresent()).toBeTruthy();
+    })
+
+    it('check that loginBtn is appeared in header', function () {
+        expect(mainPage.header.loginBtn.isPresent()).toBeTruthy();
+    })
+
+    it('check that signUpBtn is appeared in header', function () {
+        expect(mainPage.header.signUpBtn.isPresent()).toBeTruthy();
+    })
 });
