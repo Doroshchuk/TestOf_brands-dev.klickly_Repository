00a79804e6544e3a01026c53f7fe74947bdf1ada
ddoroshchuk <doroshukdashuly@mail.ru>
2018-04-09 21:33:57 +0300

    added test-case to log in with: 1.correct shopify using incorrect email and password - border color of fields is changed

--- tests/LogInTests_spec.js
+++ tests/LogInTests_spec.js
@@ -23,54 +23,67 @@ describe('Login at https://brands-dev.klickly.com with shopify, using email / pa
         }
     });
 
-    it('check that "logIn" page is correct', function () {
-        expect(logInPage.title.getText()).toBe("Log in") && expect(logInPage.mainText.getText()).toBe("Please log in into your account");
-    });
+    // it('check that "logIn" page is correct', function () {
+    //     expect(logInPage.title.getText()).toBe("Log in") && expect(logInPage.mainText.getText()).toBe("Please log in into your account");
+    // });
+    //
+    // // поле "email" в фокусе
+    // it('input correct shopify', function () {
+    //     Helpers.logInWithShopify("natalia-payment-store");
+    //     expect(shopifyPage.emailTF.getAttribute('id')).toEqual(browser.driver.switchTo().activeElement().getAttribute('id'));
+    // });
+    //
+    // // приветствие на странице пользователя
+    // it('log in with correct shopify using correct email and password', function () {
+    //     Helpers.logInWithShopify("natalia-payment-store");
+    //     Helpers.logInUsingEmailAndPassword("natalia.roshchyna@itrexgroup.com", "qwerty1234567");
+    //     browser.driver.sleep(20000);
+    //     userPage.isAuthenticated = true;
+    //     // Helpers.closePopUpInUserPage();
+    //     expect(userPage.mainContent.title.getText()).toBe("Welcome, Nata!");
+    // });
+    //
+    // // поле "email" выделено розовым цветом
+    // it('log in with correct shopify using incorrect email and password', function () {
+    //     Helpers.logInWithShopify("natalia-payment-store");
+    //     Helpers.logInUsingEmailAndPassword("gfhd@gmail.com", "hfgf");
+    //     expect(shopifyPage.emailTF.getCssValue('background-color')).toEqual("rgba(251, 234, 229, 1)");
+    // });
+    //
+    // // поле "password" выделено розовым цветом
+    // it('log in with correct shopify using incorrect email and password', function () {
+    //         Helpers.logInWithShopify("natalia-payment-store");
+    //         Helpers.logInUsingEmailAndPassword("gfhd@gmail.com", "kfjgh");
+    //         expect(shopifyPage.password.getCssValue('background-color')).toEqual("rgba(251, 234, 229, 1)");
+    //     });
+    //
+    // // поле "email" в фокусе
+    // it('log in with correct shopify using empty email and password', function () {
+    //     Helpers.logInWithShopify("natalia-payment-store");
+    //     Helpers.logInUsingEmailAndPassword("", "");
+    //     expect(shopifyPage.emailTF.getAttribute('id')).toEqual(browser.driver.switchTo().activeElement().getAttribute('id'));
+    // });
+    //
+    // //поля "email" и "password" выделены розовым цветом, но поле "email" будет содержать введённое пользователем значение
+    // it('log in with correct shopify using correct email and incorrect password', function () {
+    //     Helpers.logInWithShopify("natalia-payment-store");
+    //     Helpers.logInUsingEmailAndPassword("natalia.roshchyna@itrexgroup.com", "");
+    //     expect(shopifyPage.emailTF.getCssValue('background-color')).toEqual("rgba(251, 234, 229, 1)")
+    //         && expect(Helpers.getTextFromElement(shopifyPage.emailTF)).toBe("natalia.roshchyna@itrexgroup.com");
+    // });
 
-    // поле "email" в фокусе
-    it('input correct shopify', function () {
-        Helpers.logInWithShopify("natalia-payment-store");
-        expect(shopifyPage.emailTF.getAttribute('id')).toEqual(browser.driver.switchTo().activeElement().getAttribute('id'));
-    });
-
-    // приветствие на странице пользователя
-    it('log in with correct shopify using correct email and password', function () {
-        Helpers.logInWithShopify("natalia-payment-store");
-        Helpers.logInUsingEmailAndPassword("natalia.roshchyna@itrexgroup.com", "qwerty1234567");
-        browser.driver.sleep(20000);
-        userPage.isAuthenticated = true;
-        // Helpers.closePopUpInUserPage();
-        expect(userPage.mainContent.title.getText()).toBe("Welcome, Nata!");
-    });
-
-    // поле "email" выделено розовым цветом
-    it('log in with correct shopify using incorrect email and password', function () {
-        Helpers.logInWithShopify("natalia-payment-store");
-        Helpers.logInUsingEmailAndPassword("gfhd@gmail.com", "hfgf");
-        expect(shopifyPage.emailTF.getCssValue('background-color')).toEqual("rgba(251, 234, 229, 1)");
-    });
-
-    // поле "password" выделено розовым цветом
+    // границы поля "password" выделены красным цветом
     it('log in with correct shopify using incorrect email and password', function () {
             Helpers.logInWithShopify("natalia-payment-store");
             Helpers.logInUsingEmailAndPassword("gfhd@gmail.com", "kfjgh");
-            expect(shopifyPage.password.getCssValue('background-color')).toEqual("rgba(251, 234, 229, 1)");
+            expect(shopifyPage.password.getCssValue('border-color')).toEqual("rgba(222, 54, 24, 1)");
         });
 
-    // поле "email" в фокусе
-    it('log in with correct shopify using empty email and password', function () {
+    // границы поля "email" выделены красным цветом
+    it('log in with correct shopify using incorrect email and password', function () {
         Helpers.logInWithShopify("natalia-payment-store");
-        Helpers.logInUsingEmailAndPassword("", "");
-        expect(shopifyPage.emailTF.getAttribute('id')).toEqual(browser.driver.switchTo().activeElement().getAttribute('id'));
+        Helpers.logInUsingEmailAndPassword("gfhd@gmail.com", "kfjgh");
+        expect(shopifyPage.emailTF.getCssValue('border-color')).toEqual("rgba(222, 54, 24, 1)");
     });
-
-    //поля "email" и "password" выделены розовым цветом, но поле "email" будет содержать введённое пользователем значение
-    it('log in with correct shopify using correct email and incorrect password', function () {
-        Helpers.logInWithShopify("natalia-payment-store");
-        Helpers.logInUsingEmailAndPassword("natalia.roshchyna@itrexgroup.com", "");
-        expect(shopifyPage.emailTF.getCssValue('background-color')).toEqual("rgba(251, 234, 229, 1)")
-            && expect(Helpers.getTextFromElement(shopifyPage.emailTF)).toBe("natalia.roshchyna@itrexgroup.com");
-    });
-
 
 });
